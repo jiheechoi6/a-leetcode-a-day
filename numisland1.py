362d61bc4a43da57ebf72f5a635d13eb0fdbe667
jiheechoi6 <52632340+jiheechoi6@users.noreply.github.com>
2021-10-06 10:20:48 +0900

    Create numisland1.py

---
+++ numisland1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def dfs(self, grid, i, j):
+        if i < 0 or i >= len(grid) or j<0 or j>=len(grid[i]) or grid[i][j] == '0':
+            return 0
+        
+        grid[i][j] = '0'
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i, j-1)
+        self.dfs(grid, i, j+1)
+            
+        return 0
+    
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if grid == None or len(grid) == 0:
+            return 0
+        
+        num_islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1':
+                    num_islands += 1
+                    
+                    self.dfs(grid, i, j)
+                    
+        return num_islands
+    
+    
