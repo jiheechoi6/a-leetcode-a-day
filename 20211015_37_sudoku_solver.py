b373c1995776145b5866466d6d449257283e7ea1
jiheechoi6 <52632340+jiheechoi6@users.noreply.github.com>
2022-02-16 14:26:06 +0900

    Update 20211015_37_sudoku_solver.py

--- 20211015_37_sudoku_solver.py
+++ 20211015_37_sudoku_solver.py
@@ -1,51 +1,45 @@
 class Solution:
-    row_used = None
-    col_used = None
-    cube_used = None
-    empty = None
-    board = None
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        self.row_used = collections.defaultdict(set)
-        self.col_used = collections.defaultdict(set)
-        self.cube_used = collections.defaultdict(set)
-        self.empty = []
+        self.board = board
+        self.rows = [set() for _ in range(9)]
+        self.cols = [set() for _ in range(9)]
+        self.cubes = [set() for _ in range(9)]
+        self.emptys = []
+        
         for i, j in product(range(9), repeat=2):
             val = board[i][j]
             if val == '.':
-                self.empty.append((i, j))
-            else:
-                self.row_used[i].add(val)
-                self.col_used[j].add(val)
-                self.cube_used[(i//3)*3+j//3].add(val)
-        self.board = board
-        self.backtrack(0) 
-        board = self.board
-        return board
+                self.emptys.append((i, j))
+            self.rows[i].add(val)
+            self.cols[j].add(val)
+            self.cubes[(i//3)*3+j//3].add(val)
+        
+        return self.backtrack(0)
     
     def backtrack(self, idx):
-        if idx == len(self.empty):  # all empty spots filled
-            return True
+        if idx == len(self.emptys):  return True
         
-        row, col = self.empty[idx]
+        i, j = self.emptys[idx]
+        c_idx = (i//3)*3+ j//3
         for cand in range(1, 10):
             cand = str(cand)
-            if cand in self.row_used[row] or cand in self.col_used[col] or cand in self.cube_used[(row//3)*3+col//3]:
+            if cand in self.rows[i] or cand in self.cols[j] or cand in self.cubes[c_idx]:
                 continue
-            else:
-                self.board[row][col] = cand
-                self.row_used[row].add(cand)
-                self.col_used[col].add(cand)
-                self.cube_used[(row//3)*3+col//3].add(cand)
-                final = self.backtrack(idx+1)
-                if final:
-                    return True
-                self.board[row][col] = '.'
-                self.row_used[row].remove(cand)
-                self.col_used[col].remove(cand)
-                self.cube_used[(row//3)*3+col//3].remove(cand)
+            
+            self.board[i][j] = cand
+            self.rows[i].add(cand)
+            self.cols[j].add(cand)
+            self.cubes[c_idx].add(cand)
+            if self.backtrack(idx+1): return True
+            self.board[i][j] = '.'
+            self.rows[i].remove(cand)
+            self.cols[j].remove(cand)
+            self.cubes[c_idx].remove(cand)
+        
+        return False
                 
         
         
