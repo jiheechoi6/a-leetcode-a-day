46494edfe79c2450f4ebd036429034408533fdd6
jiheechoi6 <52632340+jiheechoi6@users.noreply.github.com>
2022-03-08 05:31:04 +0900

    Update 139_word_break.py

--- 139_word_break.py
+++ 139_word_break.py
@@ -1,10 +1,10 @@
 class Solution:
     def wordBreak(self, s: str, words: List[str]) -> bool:
-        ok = [True]
-        max_len = max(map(len, words+['']))
+        dp = [True]
         words = set(words)
-        for i in range(1, len(s)+1):
-            ok += any(ok[j] and s[j:i] in words for j in range(max(0, i-max_len),i)),
-        return ok[-1]
+        max_len = max(map(len, words))
         
+        for i, ch in enumerate(s):
+            dp.append(any(s[j:i+1] in words and dp[j] for j in range(max(0, i-max_len), i+1)))
         
+        return dp[-1]
