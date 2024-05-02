8e2bccba5f2f6838aed2ae7057cf89235102ea78
jiheechoi6 <52632340+jiheechoi6@users.noreply.github.com>
2024-05-02 14:20:25 -0400

    Create 1650_lowest_common_ancestor_of_a_binary_tree.py

---
+++ 1650_lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        path = set()
+
+        while p:
+            path.add(p.val)
+            p = p.parent
+
+        while q.val not in path:
+            q = q.parent
+
+        return q
+      
