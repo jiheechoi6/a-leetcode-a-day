3b1d3dd93c54c4e530abb00db38496bc630592de
jiheechoi6 <52632340+jiheechoi6@users.noreply.github.com>
2024-06-03 21:04:09 -0400

    Update 983_minimum_cost_for_tickets.java

--- 983_minimum_cost_for_tickets.java
+++ 983_minimum_cost_for_tickets.java
@@ -1,23 +1,24 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        int[] dp = new int[days.length+1];
-        dp[0] = 0;
+        int[] dp = new int[days.length];
+        dp[0] = costs[0];
 
-        for(int i = 1; i <= days.length; i++){
-            int pass1 = dp[i-1] + costs[0];
+        for(int i = 1; i < days.length; i++){
+            int cost1 = dp[i-1] + costs[0];
 
             int lastUncovered = i-1;
-            while(lastUncovered >= 0 && (days[i-1] - days[lastUncovered]) < 7) lastUncovered--;
-
-            int pass7 = costs[1] + dp[lastUncovered+1];
-
+            while(lastUncovered != -1 && (days[i] - days[lastUncovered]) < 7) lastUncovered--;
+            int cost7 = costs[1];
+            if(lastUncovered != -1) cost7 += dp[lastUncovered]; 
+            
             lastUncovered = i-1;
-            while(lastUncovered >= 0 && (days[i-1] - days[lastUncovered]) < 30) lastUncovered--;
-            int pass30 = costs[2] + dp[lastUncovered+1];
+            while(lastUncovered != -1 && (days[i] - days[lastUncovered]) < 30) lastUncovered--;
+            int cost30 = costs[2];
+            if(lastUncovered != -1) cost30 += dp[lastUncovered]; 
 
-            dp[i] = Math.min(pass1, Math.min(pass7, pass30));
+            dp[i] = Math.min(cost1, Math.min(cost7, cost30));
         }
 
-        return dp[days.length];
+        return dp[days.length - 1];
     }
 }
