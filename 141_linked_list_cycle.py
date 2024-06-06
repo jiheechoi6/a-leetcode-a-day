51ee1e6b95cb13c422b6bb859c9f3069b45284e3
jiheechoi6 <52632340+jiheechoi6@users.noreply.github.com>
2024-06-05 21:03:51 -0400

    Create 141_linked_list_cycle.py

---
+++ 141_linked_list_cycle.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        walker, runner = head, head
+
+        while runner and runner.next:
+            walker = walker.next
+            runner = runner.next.next
+
+            if walker == runner:
+                return True
+        
+        return False
+        
