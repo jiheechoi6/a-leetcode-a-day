0f19665a173bfe07075ef29e6ff2af1e61a1c6bc
jiheechoi6 <52632340+jiheechoi6@users.noreply.github.com>
2024-06-07 17:11:32 -0400

    Update 15_3sum.py

--- 15_3sum.py
+++ 15_3sum.py
@@ -1,25 +1,18 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # last_idx = len(nums)-1
-        result = []
-        nums.sort()
-        for i in range(len(nums)-2):
-            if i>0 and nums[i] == nums[i-1]:  # skip duplicate
-                continue
-            l = i+1
-            r = len(nums)-1
-            while l<r:
-                s = nums[l]+nums[r]
-                if s < -nums[i]:
-                    l+=1
-                elif s > -nums[i]:
-                    r-=1
-                else:
-                    result.append([nums[i], nums[l], nums[r]])
-                    while l<r and nums[l]==nums[l+1]:
-                        l+=1
-                    while l<r and nums[r]==nums[r-1]:
-                        r-=1
-                    l+=1
-                    r-=1
-        return result
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s: return 0
+
+        history = {s[0]: 0}
+        cur_len = 1
+        max_len = 1
+
+        for i in range(1, len(s)):
+            if s[i] in history and (i - cur_len) <= history[s[i]]:
+                cur_len = i - history[s[i]]
+            else:
+                cur_len += 1
+            
+            max_len = max(max_len, cur_len)
+            history[s[i]] = i
+
+        return max_len
