9cc555527b7e18609811bfc90a22f04e89caa736
jiheechoi6 <52632340+jiheechoi6@users.noreply.github.com>
2024-06-09 16:35:53 -0400

    Update 200_number_of_islands_1.py

--- 200_number_of_islands_1.py
+++ 200_number_of_islands_1.py
@@ -1,28 +1,19 @@
 class Solution:
-    def dfs(self, grid, i, j):
-        if i < 0 or i >= len(grid) or j<0 or j>=len(grid[i]) or grid[i][j] == '0':
-            return 0
-        
-        grid[i][j] = '0'
-        self.dfs(grid, i-1, j)
-        self.dfs(grid, i+1, j)
-        self.dfs(grid, i, j-1)
-        self.dfs(grid, i, j+1)
-            
-        return 0
-    
     def numIslands(self, grid: List[List[str]]) -> int:
-        if grid == None or len(grid) == 0:
-            return 0
-        
-        num_islands = 0
+        m, n = len(grid), len(grid[0])
+        def dfs(row, col):
+            grid[row][col] = "0"
+
+            for i, j in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                newrow, newcol = i + row, j + col
+                if 0<= (newrow) < m and 0 <= (newcol) < n and grid[newrow][newcol] == '1':
+                    dfs(newrow, newcol)
+
+        count = 0
         for i in range(len(grid)):
-            for j in range(len(grid[i])):
+            for j in range(len(grid[0])):
                 if grid[i][j] == '1':
-                    num_islands += 1
-                    
-                    self.dfs(grid, i, j)
-                    
-        return num_islands
-    
-    
+                    count += 1
+                    dfs(i, j)
+        
+        return count
