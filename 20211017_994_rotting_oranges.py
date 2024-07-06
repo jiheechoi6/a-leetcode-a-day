5c9f8f73c989e9c2c8ba8aa72dba12fedd44becb
jiheechoi6 <52632340+jiheechoi6@users.noreply.github.com>
2024-07-05 20:15:05 -0400

    Update 20211017_994_rotting_oranges.py

--- 20211017_994_rotting_oranges.py
+++ 20211017_994_rotting_oranges.py
@@ -1,40 +1,29 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        if len(grid) == 0: return 0
-        
-        dirc = {(-1, 0), (1, 0), (0, -1), (0, 1)}
-        
-        m = len(grid)
-        n = len(grid[0])
-        q = []
-        freshnum = 0
-        
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    freshnum+= 1
-                elif grid[i][j] == 2:
-                    q.append((i, j))
-        
-        minutes = 0
-        while len(q) != 0 and freshnum != 0:
-            minutes += 1
-            new_rottens = []
-            for rotten in q:
-                for d in dirc:
-                    new_row = rotten[0] + d[0]
-                    new_col = rotten[1] + d[1]
-                    if new_row>=0 and new_row<m and new_col>=0 and new_col<n and grid[new_row][new_col] == 1:
-                        freshnum -=1
-                        grid[new_row][new_col] = 2
-                        new_rottens.append((new_row, new_col))
-                        
-            q = new_rottens
+        freshCnt = 0
+        que = deque()
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1: freshCnt +=1
+                if grid[r][c] == 2: que.append((r, c))
+
+        res = 0
+        while que and freshCnt > 0:
+            size = len(que)
+
+            for _ in range(size):
+                cont = que.popleft()
+                for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                    newr, newc = cont[0] + dr, cont[1] + dc
+                    if 0 <= newr < len(grid) and 0 <= newc < len(grid[0]) and grid[newr][newc] == 1:
+                        grid[newr][newc] = 2
+                        freshCnt -= 1
+                        que.append((newr, newc))
             
-        if freshnum != 0:
-            return -1
-            
-        return minutes
-                    
-                    
-                
+            res += 1
+        
+        if freshCnt != 0: return -1
+
+        return res
+        
