187a51acdb855ab0cbb69671b9056a2df47673f8
jiheechoi6 <52632340+jiheechoi6@users.noreply.github.com>
2024-07-06 19:42:28 -0400

    Update 253_meeting_rooms2.py

--- 253_meeting_rooms2.py
+++ 253_meeting_rooms2.py
@@ -1,12 +1,12 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda item: item[0])
-        ends = [intervals[0][1]]
-        ans = 1
-        for start, end in intervals[1:]:
-            if start < ends[0]:  # new room
-                ans+=1
-                heapq.heappush(ends, end)
+        intervals = sorted(intervals, key = lambda item: item[0])
+        ends = [0]
+
+        for start, end in intervals:
+            if start < ends[0]: 
+                heappush(ends, end)
             else:
-                heapq.heapreplace(ends, end)
-        return ans
+                heapreplace(ends, end)
+        
+        return len(ends)
