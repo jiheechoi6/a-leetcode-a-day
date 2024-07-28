d4c5f7a9e8c4a2007d4cb332c6f57204a2af11c2
jiheechoi6 <52632340+jiheechoi6@users.noreply.github.com>
2024-07-27 22:22:43 -0400

    Update 17_letter_combination_of_phone_number.py

--- 17_letter_combination_of_phone_number.py
+++ 17_letter_combination_of_phone_number.py
@@ -1,15 +1,21 @@
 class Solution:
-    m = [None, None, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+    def __init__(self):
+        self.letters = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+
     def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits) == 0:
+        if not digits:
             return []
+
+        res = []
+        self.dfs(digits, 0, "", res)
+
+        return res
+    
+    def dfs(self, digits, idx, cur, res):
+        if idx == len(digits): 
+            res.append(cur)
+            return
+
+        for l in self.letters[int(digits[idx])]:
+            self.dfs(digits, idx+1, cur+l, res)
         
-        ans = ['']
-        for num in digits:
-            temp = []
-            for ch in self.m[int(num)]:
-                for elem in ans:
-                    temp.append(elem+ch)
-            ans = temp
-                    
-        return ans
